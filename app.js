bde8bdda7b51ccb366d3c1a751671d4a053042d8
giuseppeesposito22 <gius.espo22@gmail.com>
2025-05-12 16:22:06 +0200

    create and start server

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("ok");
+const express = require("express");
+const { posts } = require("./posts");
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.send("Server del mio blog");
+});
+
+app.get("/bacheca", (req, res) => {
+  res.json({ posts });
+});
+
+app.listen(port, () => {
+  console.log(`Collegato alla porta http://localhost:${port}`);
+});
